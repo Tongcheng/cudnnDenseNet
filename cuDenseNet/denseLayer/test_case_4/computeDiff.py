d15f1671df17daa551de008dea049978e7bc2204
Tongcheng Li <tl486@cornell.edu>
2016-12-30 22:47:48 +0800

    CLR, BN2Bwd cause big channel-wise difference in error, worth investigating

--- cuDenseNet/denseLayer/test_case_4/computeDiff.py
+++ cuDenseNet/denseLayer/test_case_4/computeDiff.py
@@ -1,9 +1,9 @@
 
-fileNameA = "BN2_py.txt"
-fileNameB = "postBN_cpp"
+fileNameA = "Filter1Grad_py.txt"
+fileNameB = "filterGrad0_cpp"
 A_offset = 0
-B_offset = 75
-rangeLen = 50
+B_offset = 0
+rangeLen = 54
 
 listA =  open(fileNameA,'r').readlines()[0].split(",")[:-1]
 listB =  open(fileNameB,'r').readlines()[0].split(',')[:-1]
@@ -12,13 +12,14 @@ floatBL = map(lambda x:float(x),listB)
 
 print floatAL[A_offset:A_offset+rangeLen]
 print floatBL[B_offset:B_offset+rangeLen]
+for i in range(rangeLen):
+    numA,numB = floatAL[A_offset+i],floatBL[B_offset+i]
+    print abs(numA-numB)
+    
 
 globalMaxDiff = 0 
 for i in range(rangeLen):
-    aIdx,bIdx = A_offset+i,B_offset+i
+    aIdx,bIdx = A_offset+i,B_offset+i 
     globalMaxDiff = max(globalMaxDiff,abs(floatAL[aIdx]-floatBL[bIdx]))
-    #test
-    aVal,bVal = floatAL[aIdx],floatBL[bIdx]
-    print abs(aVal - bVal) 
 
 print "global Max Diff is:"+`globalMaxDiff`
